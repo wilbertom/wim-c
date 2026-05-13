d2be385173d9a887e8b95e76aa0a4a805e12e5fd
Wilberto Morales <wilbertomorales777@gmail.com>
2026-05-13 16:33:55 -0500

    Add git library

---
+++ clis/gitignore/src/git.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "git.h"
+
+#include "string.h"
+#include "subprocess.h"
+
+void git_clone(const char *url, const char *destination) {
+    string *command = string_new();
+    string_append(command, "git clone ");
+    string_append(command, url);
+    string_append(command, " ");
+    string_append(command, destination);
+
+    subprocess *process = subprocess_run(string_c_string(command));
+
+    string_free(command);
+    subprocess_free(process);
+}
+
+void git_pull(__unused const char *repository) {
+
+}
---
+++ clis/gitignore/src/git.h
@@ -0,0 +1,7 @@
+#ifndef GIT
+#define GIT
+
+void git_clone(const char *url, const char *destination);
+void git_pull(const char *repository);
+
+#endif
